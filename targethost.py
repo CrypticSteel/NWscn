905f05a0cbc3d817ed5dc17655db551840bcc5fd
Crypticsteel <phelps.t.justin@gmail.com>
2025-07-14 22:21:34 +0000

    added to scanner

--- targethost.py
+++ targethost.py
@@ -1,7 +1,30 @@
 import socket
 
-def target():
-    ip = input ("Enter IP or FQDN: ")
-    print(f"Scanning: {ip}")
+###def target():
+    #trgt = input ("Enter IP or name: ")
+    #print(f"Scanning: {trgt}")
 
-target()
+def ScanPort(target, port):
+    
+    try:
+        #creating socket object
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        
+        #set a timeout
+            s.settimeout(1.0)
+
+        #Try connecting - returns 0 if successful
+            result = s.connect_ex((target, port))
+
+        #Clean Shutdown
+            s.shutdown(socket.SHUT_RDWR)
+
+            return result == 0
+
+    except socket.timeout:
+        return False
+    except socket.error:
+        return False
+#target()
+
+print(ScanPort("scanme.nmap.org", 80))
